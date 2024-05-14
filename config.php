620f451ca601077624bbe47e37cd286100d2ea0b
salahouddiz21 <145713648+salahouddiz21@users.noreply.github.com>
2024-05-14 15:51:35 +0100

    a

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+$dns = "mysql:host=localhost;dbname=blog_db";
+$bduser ="root";
+$bdpass ="";
+
+try {
+    echo " conn err";
+    $conn = new PDO($dns,$bduser,$bdpass);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // echo "<br>Connected successfully";
+  } catch(PDOException $e) {
+    echo "Connection failed:" . $e->getMessage();
+  }
+?>
