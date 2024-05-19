544070770674846a22a57d015bee1df04aa89264
ridachaanoun <162296787+ridachaanoun@users.noreply.github.com>
2024-05-19 22:57:39 +0100

    header.php

--- blog/includes/header.php
+++ blog/includes/header.php
@@ -34,7 +34,7 @@ session_start();
             <?php else: ?>
                 <ul style="align-items: center; display: flex;">
                     <li class="inline-flex">
-                        <a href="Admin-panel">
+                       <a href="dashboard/src/setting/profile.php">
                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" style="margin-left:5px;" class="sc-d806251e-0 dQZEIF">
                                 <path d="M12 21a9 9 0 100-18 9 9 0 000 18z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="colorStroke"></path>
                                 <path d="M17 19v-1.54c0-.786-.281-1.538-.781-2.093-.5-.555-1.178-.867-1.886-.867H9.667c-.708 0-1.386.312-1.886.867A3.131 3.131 0 007 17.459V19M12 11.5a3 3 0 100-6 3 3 0 000 6z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="colorStroke"></path>
